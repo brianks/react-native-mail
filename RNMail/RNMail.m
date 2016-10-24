0196226d627e69d86fee6a6ce1ee5041e3345240
brianks <layananipromart@gmail.com>
2016-10-25 02:56:06 +0700

    Update RNMail.m

--- RNMail/RNMail.m
+++ RNMail/RNMail.m
@@ -99,6 +99,8 @@ RCT_EXPORT_METHOD(mail:(NSDictionary *)options
                 mimeType = @"text/vcard";
             } else if ([attachmentType isEqualToString:@"json"]) {
                 mimeType = @"application/json";
+            } else if ([attachmentType isEqualToString:@"octet"]) {
+                mimeType = @"application/octet-stream";
             }
 
             // Add attachment
